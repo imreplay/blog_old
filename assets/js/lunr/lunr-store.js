3a6731fc62c8ad2ae0a2cd80411f89ae97ae1977
imreplay <imreplay@imreplay.xyz>
2018-06-27 09:35:58 +0000

    Updating to imreplay/blog@9c7ec6505ac9890d37e7b39a8a5ebf88c9116659.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "hello World!!",
+        "excerpt":"hello world!! ","categories": ["getstart"],
+        "tags": [],
+        "url": "https://blog.imreplay.xyz/getstart/HelloWorld/",
+        "teaser":null}]
